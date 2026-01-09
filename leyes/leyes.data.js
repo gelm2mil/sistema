de42c6c9571bdb65de6a0e87c7eae84bafeddc17
gelm2mil <nerox2mil@proton.me>
2026-01-08 22:35:50 -0600

    Update leyes.data.js

--- leyes/leyes.data.js
+++ leyes/leyes.data.js
@@ -1,12 +1,22 @@
-const LEYES = [
+LEYES_DATA.push(
   {
-    ley: "Código Penal",
-    articulo: "Artículo 449",
-    texto: "Comete el delito de tráfico de influencias quien valiéndose de su cargo influyere en otro funcionario..."
+    ley: "Constitución Política de la República de Guatemala",
+    articulo: "Artículo 1",
+    texto: "Protección a la persona. El Estado de Guatemala se organiza para proteger a la persona y a la familia; su fin supremo es la realización del bien común."
   },
   {
-    ley: "Ley de Tránsito",
-    articulo: "Artículo 20",
-    texto: "Todo conductor deberá portar licencia vigente. La no licencia será sancionada conforme a la ley."
+    ley: "Constitución Política de la República de Guatemala",
+    articulo: "Artículo 2",
+    texto: "Deberes del Estado. Es deber del Estado garantizarle a los habitantes de la República la vida, la libertad, la justicia, la seguridad, la paz y el desarrollo integral de la persona."
+  },
+  {
+    ley: "Constitución Política de la República de Guatemala",
+    articulo: "Artículo 3",
+    texto: "Derecho a la vida. El Estado garantiza y protege la vida humana desde su concepción, así como la integridad y la seguridad de la persona."
+  },
+  {
+    ley: "Constitución Política de la República de Guatemala",
+    articulo: "Artículo 4",
+    texto: "Libertad e igualdad. En Guatemala todos los seres humanos son libres e iguales en dignidad y derechos."
   }
-];
+);
